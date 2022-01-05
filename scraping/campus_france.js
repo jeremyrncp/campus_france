8b535136ecd5923cce7054b7ad210e3de656e68a
Jérémy Gaultier <contact@gaultierweb.com>
2022-01-05 14:47:13 +0000

    all files

---
+++ scraping/campus_france.js
@@ -0,0 +1,27 @@
+describe('Login', () => {
+    it('Login to Etudes en France', () => {
+        let login = ''
+        let password = ''
+
+        cy.visit('https://pastel.diplomatie.gouv.fr/etudesenfrance/dyn/public/authentification/login.html')
+
+        cy.get('#username-ariaLabel')
+            .siblings()
+            .type(login)
+
+        cy.get('#username-ariaLabel')
+            .parent()
+            .find("input[type='password']")
+            .type(password)
+
+        cy.get("input[class='submit']")
+            .click()
+            .wait(5)
+    })
+})
+
+describe('Scrap informations', () => {
+    it('first level', () => {
+
+    })
+})
