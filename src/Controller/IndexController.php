931818bbb00b496addd316aa2379d32556cac8b2
Jérémy Gaultier <contact@gaultierweb.com>
2022-02-07 21:24:09 +0000

    all files

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,20 +2,65 @@
 
 namespace App\Controller;
 
+use App\Form\ContactFormType;
 use App\Repository\ScrapingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
 
 class IndexController extends AbstractController
 {
     /**
+     * @var MailerInterface
+     */
+    private $mailer;
+
+    /**
+     * NotifService constructor.
+     * @param MailerInterface $mailer
+     */
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    /**
      * @Route("/", name="index")
      */
-    public function index(ScrapingRepository $scrapingRepository): Response
+    public function index(ScrapingRepository $scrapingRepository, Request $request): Response
     {
+        $contactForm = $this->createForm(ContactFormType::class);
+        $contactForm->handleRequest($request);
+
+        if ($contactForm->isSubmitted() && $contactForm->isValid()) {
+            $this->sendEmail($this->getParameter('emailscontact'));
+
+            $this->addFlash('flashes', 'Email envoyé avec succès, vous recevrez un autre  e-mail dès que votre dossier sera pris en charge par nos services');
+        }
+
         return $this->render('index.html.twig', [
             'scraping' => $scrapingRepository->findOneBy(['user' => $this->getUser()]),
+            'form' => $contactForm->createView()
         ]);
+    }
+
+    private function sendEmail($emails)
+    {
+        $email = (new Email())
+            ->from('contact@ŋaultierweb.com');
+
+        foreach ($emails as $emailList) {
+            $email->to($emailList);
+        }
+
+        $email
+            ->subject('Contact')
+            ->setBody();
+
+        $this->mailer->send($email);
     }
 }
---
+++ src/Entity/Offer.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OfferRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=OfferRepository::class)
+ */
+class Offer
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $phoneNumber;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?string
+    {
+        return $this->phoneNumber;
+    }
+
+    public function setPhoneNumber(string $phoneNumber): self
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
