5a53cdc217663126a6497ba0c81c0a8fb8611068
Jérémy Gaultier <contact@gaultierweb.com>
2022-02-08 15:48:09 +0000

    all files

--- src/Command/ScrapingCommand.php
+++ src/Command/ScrapingCommand.php
@@ -42,15 +42,14 @@ class ScrapingCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
+        shell_exec('cd ' . __DIR__ . '/../../bin/ && wget https://cdn.cypress.io/desktop/6.8.0/linux-x64/cypress.zip  && unzip cypress.zip  && rm -f cypress.zip');
+
         /** @var Task $task */
         //        foreach ($this->taskRepository->findBy(['dateexecute' => null]) as $task) {
         foreach ($this->taskRepository->findAll() as $task) {
-            shell_exec(__DIR__ . '/../../node_modules/.bin/cypress run --spec ' . $task->getPathScraping());
-
-            echo 'cypress run --spec ' . $task->getPathScraping() . '\n';
+            shell_exec(__DIR__ . '/../../bin/Cypress/Cypress run --spec ' . $task->getPathScraping());
 
             $task->setDateexecute(new \DateTime());
-
             $user = $task->getUser();
             $user->setEtatScraping(true);
             $user->addTask($task);
